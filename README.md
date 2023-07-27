f5c82f77e03b9b6aaf46a5044c9c9a98125fc4c3
ArthurOuaknine <arthur.ouaknine@gmail.com>
2023-07-27 14:11:08 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# OpenForest
+
+![openforest_logo](./images/logo_wo_background.png)
+
+This repo is under construction.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='OpenForest',
+      version='0.1.0',
+      license='Apache 2.0',
+      author='Arthur Ouaknine',
+      url='https://github.com/RolnickLab/OpenForest.git',
+      packages=find_packages())
